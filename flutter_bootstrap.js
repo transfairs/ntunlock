39cc33a52c6dedf2857ad8985dcb1791b02fa5cd
transfairs <transfairs@users.noreply.github.com>
2025-05-14 02:45:00 +0000

    deploy: c8d914c95cbc2d2f6683c0f87a12597933f5e620

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2716747827"
+    serviceWorkerVersion: "201634963"
   }
 });
